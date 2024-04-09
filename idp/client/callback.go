ba362f84136183840074edef2fc72c65916d48f6
k-narusawa <narusawakohei@team-lab.com>
2024-04-09 17:02:06 +0900

    refactor: 💡 画面上にトークンを表示させる

--- idp/client/callback.go
+++ idp/client/callback.go
@@ -1,6 +1,16 @@
 package client
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"idp/models"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
 
 func CallbackHandler(c echo.Context) error {
 	code := c.QueryParam("code")
@@ -9,5 +19,39 @@ func CallbackHandler(c echo.Context) error {
 		return c.File("resources/error.html")
 	}
 
-	return c.File("resources/success.html")
+	values := url.Values{}
+	values.Set("code", code)
+	values.Add("redirect_uri", "http://localhost:3846/callback")
+	values.Add("grant_type", "authorization_code")
+	te := "http://localhost:3846/oauth2/token"
+
+	req, _ := http.NewRequest(http.MethodPost, te, strings.NewReader(values.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth("my-client", "foobar")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return c.File("resources/error.html")
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var tr models.TokenResponse
+	err = json.Unmarshal(b, &tr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.JSON(http.StatusOK, tr)
 }
---
+++ idp/models/token_response.go
@@ -0,0 +1,10 @@
+package models
+
+type TokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	IdToken      string `json:"id_token"`
+	TokenType    string `json:"token_type"`
+	ExpiresIn    int    `json:"expires_in"`
+	Scope        string `json:"scope"`
+}
