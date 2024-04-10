1cc41bd43f7777a3a3e7769619bc90ac9e2a2525
k-narusawa <narusawakohei@team-lab.com>
2024-04-10 16:41:18 +0900

    refactor: 💡 アクセストークンの変数名修正

--- idp/models/access_token.go
+++ idp/models/access_token.go
@@ -26,62 +26,62 @@ type AccessToken struct {
 	grantedAudience   string    `gorm:"type:varchar(255);not null"`
 }
 
-func (a *AccessToken) SetID(id string) {
-	a.clientID = id
+func (at *AccessToken) SetID(id string) {
+	at.clientID = id
 }
 
-func (a *AccessToken) GetID() string {
-	return a.clientID
+func (at *AccessToken) GetID() string {
+	return at.clientID
 }
 
-func (a *AccessToken) GetRequestedAt() time.Time {
-	return a.requestedAt
+func (at *AccessToken) GetRequestedAt() time.Time {
+	return at.requestedAt
 }
 
-func (a *AccessToken) GetClient() fosite.Client {
-	return fosite.Client(&Client{ID: a.clientID})
+func (at *AccessToken) GetClient() fosite.Client {
+	return fosite.Client(&Client{ID: at.clientID})
 }
 
-func (a *AccessToken) GetRequestedScopes() fosite.Arguments {
-	return strings.Split(a.scope, " ")
+func (at *AccessToken) GetRequestedScopes() fosite.Arguments {
+	return strings.Split(at.scope, " ")
 }
 
-func (a *AccessToken) GetRequestedAudience() fosite.Arguments {
-	return strings.Split(a.requestedAudience, " ")
+func (at *AccessToken) GetRequestedAudience() fosite.Arguments {
+	return strings.Split(at.requestedAudience, " ")
 }
 
-func (a *AccessToken) SetRequestedScopes(scopes fosite.Arguments) {
-	a.scope = strings.Join(scopes, " ")
+func (at *AccessToken) SetRequestedScopes(scopes fosite.Arguments) {
+	at.scope = strings.Join(scopes, " ")
 }
 
-func (a *AccessToken) SetRequestedAudience(audience fosite.Arguments) {
-	a.requestedAudience = strings.Join(audience, " ")
+func (at *AccessToken) SetRequestedAudience(audience fosite.Arguments) {
+	at.requestedAudience = strings.Join(audience, " ")
 }
 
-func (a *AccessToken) AppendRequestedScope(scope string) {
-	a.scope = a.scope + " " + scope
+func (at *AccessToken) AppendRequestedScope(scope string) {
+	at.scope = at.scope + " " + scope
 }
 
-func (a *AccessToken) GetGrantedScopes() fosite.Arguments {
-	return strings.Split(a.GrantedScope, " ")
+func (at *AccessToken) GetGrantedScopes() fosite.Arguments {
+	return strings.Split(at.GrantedScope, " ")
 }
 
-func (a *AccessToken) GetGrantedAudience() fosite.Arguments {
-	return strings.Split(a.grantedAudience, " ")
+func (at *AccessToken) GetGrantedAudience() fosite.Arguments {
+	return strings.Split(at.grantedAudience, " ")
 }
 
-func (a *AccessToken) GrantScope(scope string) {
-	a.GrantedScope = a.GrantedScope + " " + scope
+func (at *AccessToken) GrantScope(scope string) {
+	at.GrantedScope = at.GrantedScope + " " + scope
 }
 
-func (a *AccessToken) GrantAudience(audience string) {
-	a.grantedAudience = a.grantedAudience + " " + audience
+func (at *AccessToken) GrantAudience(audience string) {
+	at.grantedAudience = at.grantedAudience + " " + audience
 }
 
-func (a *AccessToken) GetSession() fosite.Session {
+func (at *AccessToken) GetSession() fosite.Session {
 	var session fosite.DefaultSession
 
-	err := json.Unmarshal([]byte(a.SessionData), &session)
+	err := json.Unmarshal([]byte(at.SessionData), &session)
 	if err != nil {
 		log.Printf("Error occurred in GetSession: %+v", err)
 		return nil
@@ -92,25 +92,25 @@ func (a *AccessToken) GetSession() fosite.Session {
 	return &session
 }
 
-func (a *AccessToken) SetSession(session fosite.Session) {
+func (at *AccessToken) SetSession(session fosite.Session) {
 	jsonData, err := json.Marshal(session)
 
 	if err != nil {
 		return
 	}
 
-	a.SessionData = string(jsonData)
+	at.SessionData = string(jsonData)
 }
 
-func (a *AccessToken) GetRequestForm() url.Values {
+func (at *AccessToken) GetRequestForm() url.Values {
 	return url.Values{}
 }
 
-func (a *AccessToken) Merge(requester fosite.Requester) {
+func (at *AccessToken) Merge(requester fosite.Requester) {
 	// Merge implementation goes here
 }
 
-func (a *AccessToken) Sanitize(allowedParameters []string) fosite.Requester {
+func (at *AccessToken) Sanitize(allowedParameters []string) fosite.Requester {
 	// Sanitize implementation goes here
 	return nil
 }
@@ -136,17 +136,17 @@ func AccessTokenOf(signature string, requester fosite.Requester) *AccessToken {
 	}
 }
 
-func (a *AccessToken) ToRequester() fosite.Requester {
+func (at *AccessToken) ToRequester() fosite.Requester {
 	return &AccessToken{
-		Signature:         a.Signature,
-		clientID:          a.clientID,
-		requestedAt:       a.requestedAt,
-		scope:             a.scope,
-		GrantedScope:      a.GrantedScope,
-		FormData:          a.FormData,
-		Active:            a.Active,
-		requestedAudience: a.requestedAudience,
-		grantedAudience:   a.grantedAudience,
-		SessionData:       a.SessionData,
+		Signature:         at.Signature,
+		clientID:          at.clientID,
+		requestedAt:       at.requestedAt,
+		scope:             at.scope,
+		GrantedScope:      at.GrantedScope,
+		FormData:          at.FormData,
+		Active:            at.Active,
+		requestedAudience: at.requestedAudience,
+		grantedAudience:   at.grantedAudience,
+		SessionData:       at.SessionData,
 	}
 }
