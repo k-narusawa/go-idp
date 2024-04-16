d57b61406a21227bbe5ce5fb50712bfd088cb7fd
narusawakohei <swkei320@gmail.com>
2024-04-16 21:03:49 +0900

    feat: 🎸 wellknowのエンドポイント追加

--- authorization/adapter/handler.go
+++ authorization/adapter/handler.go
@@ -26,4 +26,36 @@ func NewOauth2Handler(e *echo.Echo, authUsecase usecase.AuthorizationUsecase, to
 	e.POST("/oauth2/token", handler.tUseCase.Invoke)
 	e.POST("/oauth2/introspect", handler.iUseCase.Invoke)
 	e.GET("/oauth2/certs", handler.jUseCase.Invoke)
+	e.GET("/.well-known/openid-configuration", wellKnownOpenIDConfiguration)
+}
+
+func wellKnownOpenIDConfiguration(c echo.Context) error {
+	return c.JSON(200, map[string]interface{}{
+		"issuer":                 "http://localhost:3846",
+		"authorization_endpoint": "http://localhost:3846/oauth2/auth",
+		"token_endpoint":         "http://localhost:3846/oauth2/token",
+		"jwks_uri":               "http://localhost:3846/oauth2/certs",
+		"response_types_supported": []string{
+			"code",
+			"token",
+			"id_token",
+			"code token",
+			"code id_token",
+			"token id_token",
+			"code token id_token",
+		},
+		"subject_types_supported": []string{
+			"public",
+		},
+		"id_token_signing_alg_values_supported": []string{
+			"RS256",
+		},
+		"scopes_supported": []string{
+			"openid",
+			"offline",
+		},
+		"token_endpoint_auth_methods_supported": []string{
+			"client_secret_basic",
+		},
+	})
 }
